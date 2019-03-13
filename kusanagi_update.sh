0a5b16dbbdd4a03114faf536bed9b633a4cda999
タネグサ <tanegusa@tanegusanoMacBook-ea.local>
2019-03-13 19:10:22 +0900

    fix kusanagi_update.sh

--- kusanagi_update.sh
+++ kusanagi_update.sh
@@ -3,6 +3,8 @@
 #データを残す日数
 SPAN=7
 
+BASE_DIR=`pwd`
+
 # wpのalias
 alias wp='sudo -u kusanagi /usr/local/bin/wp'
 
@@ -12,8 +14,8 @@ for d in `ls /home/kusanagi`; do
 	cd /home/kusanagi/$d/DocumentRoot;
 
     # データのバックアップ
-    if [ ! -e backup/$d ]; then
-        mkdir backup/$d;
+    if [ ! -e $BASE_DIR/backup/$d ]; then
+        mkdir $BASE_DIR/backup/$d;
     fi 
     
     db_name=`cat wp-config.php|grep DB_NAME|cut -d"'" -f4`
@@ -22,11 +24,11 @@ for d in `ls /home/kusanagi`; do
     db_password=`cat wp-config.php|grep DB_PASSWORD|cut -d"'" -f4`
 
     dt=`date '+%Y%m%d'`
-    mysqldump -u$db_user -p$db_password -h$db_host $db_name > $db_name_dump_$dt.dump
+    mysqldump -u$db_user -p$db_password -h$db_host $db_name > $BASE_DIR/backup/$d/$dt.dump
 
     # データを残す期間以上に残していたら一番古いものを削除
-    if [ `ls backup/$d| wc -l` -ge $SPAN ]; then
-        rm -f backup/$d/`ls backup/$d|head -1`;
+    if [ `ls $BASE_DIR/backup/$d| wc -l` -ge $SPAN ]; then
+        rm -f $BASE_DIR/backup/$d/`ls $BASE_DIR/backup/$d|head -1`;
     fi
 
     # update
