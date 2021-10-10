8a40ebc052991ed7123ab30bac4bdb144b2edaf9
umentu <s.tachibana@umentulab.com>
2021-10-10 22:31:09 +0900

    Update kusanagi_update.sh

--- kusanagi_update.sh
+++ kusanagi_update.sh
@@ -20,9 +20,11 @@ for d in `ls /home/kusanagi|grep -v kusanagi_update`; do
     db_host=`cat wp-config.php|grep DB_HOST|cut -d"'" -f4`
     db_password=`cat wp-config.php|grep DB_PASSWORD|cut -d"'" -f4`
 
+    cd $BASE_DIR/backup/$d
     dt=`date '+%Y%m%d'`
-    mysqldump -u$db_user -p$db_password -h$db_host $db_name > $BASE_DIR/backup/$d/$dt.dump
-
+    mysqldump -u$db_user -p$db_password -h$db_host $db_name > $dt.dump
+    tar -jcvf $dt.tar.bz2 $BASE_DIR/$d
+    
     # データを残す期間以上に残していたら一番古いものを削除
     if [ `ls $BASE_DIR/backup/$d| wc -l` -ge $SPAN ]; then
         rm -f $BASE_DIR/backup/$d/`ls $BASE_DIR/backup/$d|head -1`;
